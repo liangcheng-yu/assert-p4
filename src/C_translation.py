a4c5a6ac022ea611d5469d2694fe2e41c11e4c97
Gabriel Martins <gabrielnmartins@gmail.com>
2018-10-24 15:15:20 -0300

    fix: now checking assertions if pipeline finished in 'verify' error

--- src/C_translation.py
+++ src/C_translation.py
@@ -428,7 +428,8 @@ def MethodCallExpression(node):
         returnString +=  "//Extern: " + toC(node.method)
     #verify method
     elif hasattr(node.method, 'path') and node.method.path.name == "verify":
-        returnString += "if(!(" + toC(node.arguments.vec[0]) + ")) { exit(1); }"
+        returnString += "if(!(" + toC(node.arguments.vec[0]) + ")) {\n"
+        returnString += "\t\tend_assertions();\n\t\texit(1);\n\t}\n"
      #SetValid method
     elif hasattr(node.method, 'member') and node.method.member == "setValid":
         returnString += toC(node.method.expr) + ".isValid = 1;"
